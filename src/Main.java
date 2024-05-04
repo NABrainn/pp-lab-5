142faf5ea7732a8754e5edac8fbe08017bf768a3
nabrain <michal2002.60@gmail.com>
2024-05-04 18:39:08 +0200

    zad1

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) throws InvalidAgeException {
         try {
             Person person = new Person("Mike", 24);
+            System.out.println("Name: " + person.getName());
+            System.out.println("Age: " + person.getAge());
         } catch (InvalidAgeException e) {
             throw new InvalidAgeException();
         }
