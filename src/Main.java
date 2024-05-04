074cf45efe667a8f79b381cc21cab1c9377bd41f
nabrain <michal2002.60@gmail.com>
2024-05-04 18:29:06 +0200

    zad2

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import company.exceptions.InvalidAgeException;
 public class Main {
     public static void main(String[] args) throws InvalidAgeException {
         try {
-            Person person = new Person("Mike", 240);
+            Person person = new Person("Mike", 24);
         } catch (InvalidAgeException e) {
             throw new InvalidAgeException();
         }
