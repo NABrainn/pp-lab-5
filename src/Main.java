c6d5ebb968514fe25b07e01e661e3cb6f9d7efba
nabrain <michal2002.60@gmail.com>
2024-05-04 17:47:32 +0200

    zad2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 import company.beans.Person;
 import company.exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
 
 public class Main {
     public static void main(String[] args) throws InvalidAgeException {
+
         try {
             Person person = new Person("Mike", 240);
         } catch (InvalidAgeException e) {
             throw new InvalidAgeException();
         }
+
+        EmailMessenger emailMessenger = new EmailMessenger();
+        emailMessenger.sendMessage("Sample message");
     }
 }
