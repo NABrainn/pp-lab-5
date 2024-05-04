4caf60fcdb30693abd65ae8cf89fc9c227e6ca14
nabrain <michal2002.60@gmail.com>
2024-05-04 17:55:44 +0200

    zad3

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
 import company.beans.Person;
 import company.exceptions.InvalidAgeException;
 import company.implementations.EmailMessenger;
+import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) throws InvalidAgeException {
 
         try {
-            Person person = new Person("Mike", 240);
+            Person person = new Person("Mike", 24);
         } catch (InvalidAgeException e) {
             throw new InvalidAgeException();
         }
 
         EmailMessenger emailMessenger = new EmailMessenger();
         emailMessenger.sendMessage("Sample message");
+
+        int sum = MathUtils.add(2, 5);
+        System.out.println("Result of adding: " + sum);
+        emailMessenger.sendMessage("Result of adding: " + sum);
     }
 }
